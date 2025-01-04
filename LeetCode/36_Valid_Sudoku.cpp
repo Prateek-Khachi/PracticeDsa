4e815e28bad5793a50c270112c8dbe9cf47e99fa
Prateek-Khachi <145461958+Prateek-Khachi@users.noreply.github.com>
2025-01-04 23:33:16 +0530

    Update 36_Valid_Sudoku.cpp

--- LeetCode/36_Valid_Sudoku.cpp
+++ LeetCode/36_Valid_Sudoku.cpp
@@ -1,27 +1,27 @@
-#include <vector>
-using namespace std;
-
 class Solution {
+
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        int rows[9] = {0}, columns[9] = {0}, boxes[9] = {0};
         
-        for (int i = 0; i < 9; ++i) {
-            for (int j = 0; j < 9; ++j) {
-                if (board[i][j] == '.') continue;
-                
-                int num = board[i][j] - '1'; 
-                int mask = 1 << num;         
-                int boxIndex = (i / 3) * 3 + j / 3;
-                
-                if (rows[i] & mask || columns[j] & mask || boxes[boxIndex] & mask) {
-                    return false;
+        int row[9][9] ;
+        int col[9][9] ;
+        int grid[9][9] ;
+
+        for(int i=0; i<9; i++){
+            for(int j=0; j<9; j++){
+
+                if(board[i][j]!='.'){
+
+                    int n = board[i][j] - '0';
+
+                    int k = i/3 *3 +j/3;
+
+                    if(row[i][n-1]++ || col[j][n-1]++ || grid[k][n-1]++){
+                        return false;
+                    }
                 }
-                
-                rows[i] |= mask;
-                columns[j] |= mask;
-                boxes[boxIndex] |= mask;
             }
+            
         }
         return true;
     }
