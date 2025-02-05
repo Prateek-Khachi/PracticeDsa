8e60e584dd6b2557b99f660dbfe918b1e205b2f0
Prateek-Khachi <145461958+Prateek-Khachi@users.noreply.github.com>
2025-02-06 00:26:08 +0530

    Create 739._Daily_Temperatures.cpp

---
+++ LeetCode/739._Daily_Temperatures.cpp
@@ -0,0 +1,40 @@
+class Solution
+{
+public:
+    vector<int> dailyTemperatures(vector<int> &temp)
+    {
+
+        stack<int> st;
+        vector<int> ans(temp.size(), 0);
+
+        for (int i = temp.size() - 1; i >= 0; i--)
+        {
+
+            if (st.empty())
+            {
+                st.push(i);
+                ans[i] = 0;
+            }
+            else
+            {
+                while (!st.empty() && temp[i] >= temp[st.top()])
+                {
+                    st.pop();
+                }
+
+                if (st.empty())
+                {
+                    ans[i] = 0;
+                }
+                else
+                {
+                    ans[i] = st.top() - i;
+                }
+
+                st.push(i);
+            }
+        }
+
+        return ans;
+    }
+};
