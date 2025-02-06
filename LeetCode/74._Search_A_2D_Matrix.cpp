cae4d0462453d4c41be16ce740137ca48a12f14e
Prateek-Khachi <145461958+Prateek-Khachi@users.noreply.github.com>
2025-02-06 20:49:23 +0530

    Update 74._Search_A_2D_Matrix.cpp

--- LeetCode/74._Search_A_2D_Matrix.cpp
+++ LeetCode/74._Search_A_2D_Matrix.cpp
@@ -1,23 +1,28 @@
-class Solution
-{
+class Solution {
 public:
-    bool searchMatrix(vector<vector<int>> &matrix, int target)
-    {
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        
         int rows = matrix.size();
         int cols = matrix[0].size();
-        int left = 0, right = rows * cols - 1;
 
-        while (left <= right)
-        {
-            int mid = left + (right - left) / 2;
-            int midValue = matrix[mid / cols][mid % cols];
-            if (midValue == target)
-                return true;
-            if (midValue < target)
-                left = mid + 1;
-            else
-                right = mid - 1;
+        int left = 0;
+        int right = cols*rows-1;
+        int mid = 0;
+        int midVal = 0;
+
+        while(left<=right){
+
+            mid = left + (right-left)/2;
+            midVal = matrix[mid/cols][mid%cols];
+
+            if(midVal==target) return true;
+
+            if(target>midVal) left = mid+1;
+            else(right = mid-1);
+
         }
+
         return false;
+
     }
 };
